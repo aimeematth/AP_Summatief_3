dcc1f304273ff55f18d2278a7974b4494ea9643d
aimeematth <lunacecima@gmail.com>
2023-06-11 18:25:50 +0200

    main veranderd

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include "include/graph.hpp"
 #include "include/node.hpp"
 #include "include/edge.hpp"
@@ -7,35 +6,35 @@
 #include "include/vlucht.hpp"
 #include <iostream>
 #include <vector>
-
 int main() {
     // Create some nodes
-    Node* nodeA = new Node("A");
-    Node* nodeB = new Node("B");
-    Node* nodeC = new Node("C");
-    Node* nodeD = new Node("D");
-
+    Node* nodeUtrecht = new Node("Utrecht");
+    Node* nodeAmersfoort = new Node("Amersfoort");
+    Node* nodeEnschede = new Node("Enschede");
+    Node* nodeAmsterdam = new Node("Amsterdam");
+    Node* nodeMarseille = new Node("Marseille");
+    Node* nodeBrussel = new Node("Brussel");
+    Node* nodeMaastricht = new Node("Maastricht");
     // Create some edges
-    Edge* edgeAB = new Edge(nodeA, nodeB, (new Auto(10))->getKosten());
-    Edge* edgeAC = new Edge(nodeA, nodeC, (new Treinrit(5))->getKosten());
-    Edge* edgeBD = new Edge(nodeB, nodeD, (new Vlucht(50))->getKosten());
-    Edge* edgeCD = new Edge(nodeC, nodeD, (new Auto(20))->getKosten());
-
+    Edge* edgeUtrechtAmersfoort = new Edge(nodeUtrecht, nodeAmersfoort, (new Auto(10))->getKosten());
+    Edge* edgeUtrechtEnschede = new Edge(nodeUtrecht, nodeEnschede, (new Treinrit(20))->getKosten());
+    Edge* edgeAmersfoortAmsterdam = new Edge(nodeAmersfoort, nodeAmsterdam, (new Auto(15))->getKosten());
+    Edge* edgeAmsterdamBrussel = new Edge(nodeAmsterdam, nodeBrussel, (new Vlucht(100))->getKosten());
+    Edge* edgeAmersfoortMaastricht = new Edge(nodeAmersfoort, nodeMaastricht, (new Treinrit(30))->getKosten());
+    Edge* edgeMaastrichtMarseille = new Edge(nodeMaastricht, nodeMarseille, (new Vlucht(200))->getKosten());
     // Create a graph and add nodes and edges
-    std::vector<Node*> nodes = {nodeA, nodeB, nodeC, nodeD};
-    std::vector<Edge*> edges = {edgeAB, edgeAC, edgeBD, edgeCD};
+    std::vector<Node*> nodes = {nodeUtrecht, nodeAmersfoort, nodeEnschede, nodeAmsterdam, nodeMarseille, nodeBrussel, nodeMaastricht};
+    std::vector<Edge*> edges = {edgeUtrechtAmersfoort, edgeUtrechtEnschede, edgeAmersfoortAmsterdam, edgeAmsterdamBrussel, edgeAmersfoortMaastricht, edgeMaastrichtMarseille};
     Graph graph(nodes, edges);
-
     // Perform some operations on the graph
-    std::vector<Node*> shortestPath = graph.findShortestPathWithDijkstra(nodeA, nodeD);
+    std::vector<Node*> shortestPath = graph.findShortestPathWithDijkstra(nodeUtrecht, nodeMarseille);
     int pathCost = graph.getCostOfPath(shortestPath);
-
     // Display the shortest path and the cost
     std::cout << "Shortest Path: ";
     for (Node* node : shortestPath) {
-        std::cout << node->label << " ";
+        std::cout << node->station << " ";
     }
     std::cout << std::endl;
     std::cout << "Path Cost: " << pathCost << std::endl;
     return 0;
-};
+}
